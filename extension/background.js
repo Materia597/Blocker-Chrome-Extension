5a3d18d98b47e493a97c06500de44736ece8b2ee
Materia597 <112108517+Materia597@users.noreply.github.com>
2025-08-21 16:35:27 -0700

    Added the search blocking functionality and tested to resolve obvious errors.

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,4 @@
-
-
+// Network blocking for images
 const updateRules = (blockedDomains) => {
     const rules = blockedDomains.map((domain, i) => ({
         id: i + 1,
@@ -33,4 +32,62 @@ chrome.storage.onChanged.addListener((changes, area) => {
     if (area == "sync" && changes.blockedDomains) {
         updateRules(changes.blockedDomains.newValue);
     }
+})
+
+
+// Keyword blocking
+
+let bannedWords = [];
+
+// sites to apply the search blocking to and the parameter they us in searches
+const sites = [
+    { hostContains: "google.com", param: "q"},
+    { hostContains: "bing.com", param: "q"},
+    { hostContains: "duckduckgo.com", param: "q"},
+    { hostContains: "youtube.com", param: "search_query"},
+    { hostContains: "amazon.com", param: "k"},
+    { hostContains: "wikipedia.org", param: "search"},
+    { hostContains: "tiktok.com", param: "q"},
+    { hostContains: "itch.io", param: "q"}
+]
+
+
+const checkAndBlock = (details) => {
+    try {
+        const url = new URL(details.url);
+
+        const site = sites.find(s => url.host.includes(s.hostContains));
+        if (!site) return;
+
+        const queryValue = url.searchParams.get(site.param);
+        if (queryValue) {
+            const lowerQuery = queryValue.toLowerCase();
+            if (bannedWords.some(word => lowerQuery.includes(word))) {
+                chrome.tabs.update(details.tabId, { url: chrome.runtime.getURL("block.html")})
+            }
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// load settings
+chrome.storage.local.get({ bannedWords: []}, (data) => {
+    bannedWords = data.bannedWords.map(w => w.toLowerCase());
+})
+
+// Update list when options change
+chrome.storage.onChanged.addListener((changes, area) => {
+    if (area === "local" && changes.bannedWords) {
+        baneedWords = changes.bannedWords.newValue.map(w => w.toLowerCase())
+    }
+})
+
+chrome.webNavigation.onBeforeNavigate.addListener(checkAndBlock, {
+    url: sites.map (s => ({hostContains: s.hostContains}))
+})
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(checkAndBlock, {
+    url: sites.map(s => ({hostContains: s.hostContains}))
 })
--- extension/options.js
+++ extension/options.js
@@ -1,10 +1,16 @@
 
+const bannedWordsTextArea = document.getElementById('banned-words')
 const blockedDomainsTextArea = document.getElementById('blocked-domains');
 
 const saveButton = document.getElementById('save-button');
 const saveStatus = document.getElementById('save-status');
 
+// loads banned words from storage
+chrome.storage.local.get({ bannedWords: []}, (data) => {
+    bannedWordsTextArea.value = data.bannedWords.join("\n")
+})
 
+// loads banned sites from storage
 chrome.storage.sync.get({ blockedDomains: []}, (data) => {
     blockedDomainsTextArea.value = data.blockedDomains.join("\n");
 });
@@ -16,7 +22,16 @@ saveButton.addEventListener("click", () => {
         .map(d => d.trim())
         .filter(d => d.length > 0);
 
+    const words = bannedWordsTextArea.value
+        .split("\n")
+        .map(w => w.trim())
+        .filter(w => w.length > 0)
     
+
+    chrome.storage.local.set({ bannedWords: words}, () => {
+        console.log("Banned words saved.")
+    })
+
     chrome.storage.sync.set({ blockedDomains: domains }, () => {
         saveStatus.innerText = "Saved!";
         setTimeout( () => {saveStatus.innerText = ""}, 1500);
